b7e69f7ee873336fa341fcdc30f42f7621fab338
Adam Cordeiro <acordeiro@twg.ca>
2016-01-11 17:16:30 -0500

    updated mobile footer style and js
    Removed the submit button from the mobile email submit form to free up
    some space. Changed  the X in the top right to collapse down to the
    original footer view if the mobile email form is expanded.

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 console.log('hello!')
 $('#start').click(function() {
 	$('#mobileFooter').css('display', 'none')
-	$('#desktopFooter').css('display', 'block')
+	$('#overlay').css('display', 'block')
 	$('.footer-email').focus()
 })
 
-//taken from 
+//function taken from /www-marsdd-com/wp-content/themes/marsdd-next/assets/js/_main.js
 $(function () {
 	$('.stickyFooter > .close').click(function () {
-		$('.stickyFooter').hide();
-		mars.removeStickyFooterPadding();
-		if (Cookies.get('stickyFooterSigned')) {
-			Cookies.set('stickyFooterClosed', true);
-		} else {
-			Cookies.set('stickyFooterClosed', true, { expires: 1 });
+		if ($(this).parent().attr("id") === 'desktopFooter' || 
+				$(this).parent().attr("id") === 'mobileFooter'){
+			$('.stickyFooter').hide()
+			mars.removeStickyFooterPadding()
+			if (Cookies.get('stickyFooterSigned')) {
+				Cookies.set('stickyFooterClosed', true)
+			} else {
+				Cookies.set('stickyFooterClosed', true, { expires: 1 })
+			}
+		} else if ($(this).parent().attr("id") === 'mobileForm'){
+			$('#overlay').css('display', 'none')
+	$('#mobileFooter').css('display', 'block')
 		}
-	});
+	})
 })
