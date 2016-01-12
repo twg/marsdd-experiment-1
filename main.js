0ffee4b6ac084856be7c17c6a2fb73ce17f5fdd2
Adam Cordeiro <acordeiro@twg.ca>
2016-01-12 15:42:51 -0500

    mobile responsive with bugs
    Android browsers seem to be working well, but having trouble in Safari
    for iOS and Chrome for iOS.
    
    Safari: After clicking the ‘Start’ button, the mobile form appears to
    be fixed to the bottom of the page and not sticky to the viewport.
    
    Chrome: After clicking the ‘Start’ button, the viewport centres on the
    email input and the div appears to be fixed in that spot on the page.
    Viewport can scroll down or up past the footer.

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
 console.log('hello!')
+
+// swap mobile sticky footers on button click
 $('#start').click(function() {
+	$('.base').css('padding-bottom', '450px')
 	$('#mobileFooter').css('display', 'none')
-	$('#overlay').css('display', 'block')
+	$('#mobileForm').css('display', 'block')
 	$('.footer-email').focus()
 })
 
 //function taken from /www-marsdd-com/wp-content/themes/marsdd-next/assets/js/_main.js
 $(function () {
 	$('.stickyFooter > .close').click(function () {
-		if ($(this).parent().attr("id") === 'desktopFooter' || 
-				$(this).parent().attr("id") === 'mobileFooter'){
-			$('.stickyFooter').hide()
-			mars.removeStickyFooterPadding()
-			if (Cookies.get('stickyFooterSigned')) {
-				Cookies.set('stickyFooterClosed', true)
-			} else {
-				Cookies.set('stickyFooterClosed', true, { expires: 1 })
-			}
+		if ($(this).parent().attr("id") === 'desktopFooter' || $(this).parent().attr("id") === 'mobileFooter'){
+			collapseFooter()
 		} else if ($(this).parent().attr("id") === 'mobileForm'){
-			$('#overlay').css('display', 'none')
-	$('#mobileFooter').css('display', 'block')
+			collapseForm()
 		}
 	})
 })
+
+var hidden = true;
+var currentlyHidden = true;
+// hide 
+$(window).on("scroll", function() {
+	//$('#mobileFooter').css('display', 'block')
+    var scrollPos = $(window).scrollTop();
+    var shouldHide = scrollPos <= 100;
+    if (shouldHide === currentlyHidden){
+    	return;
+    }
+    var jqueryOp = shouldHide ? "hide" : "show"
+    currentlyHidden = jqueryOp == "hide"
+    console.log(jqueryOp, scrollPos, currentlyHidden)
+    $("#mobileFooter")[jqueryOp]();
+});
+
+$('#mobile-email').focusout(function(){
+	collapseForm()
+})
+
+function collapseFooter(){
+	$('.stickyFooter').hide()
+	if (Cookies.get('stickyFooterSigned')) {
+		Cookies.set('stickyFooterClosed', true)
+	} else {
+		Cookies.set('stickyFooterClosed', true, { expires: 1 })
+	}
+}
+
+function collapseForm(){
+	$('#mobileForm').css('display', 'none')
+	$('#mobileFooter').css('display', 'block')
+}
